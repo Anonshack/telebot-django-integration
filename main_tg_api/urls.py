5131aa3ced259fa01ea629f31c91dce7fbff3951
Anonshack <anonshack48@gmail.com>
2025-11-15 10:30:13 +0500

    update

--- main_tg_api/urls.py
+++ main_tg_api/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from .views import (
+    BotUsersListCreate, BotUsersDetail,
+    FeedbackListCreate, FeedbackDetail
+)
 
 urlpatterns = [
+    path("users/", BotUsersListCreate.as_view(), name="users-list-create"),
+    path("users/<int:pk>/", BotUsersDetail.as_view(), name="users-detail"),
 
+    path("feedbacks/", FeedbackListCreate.as_view(), name="feedbacks-list-create"),
+    path("feedbacks/<int:pk>/", FeedbackDetail.as_view(), name="feedbacks-detail"),
 ]
